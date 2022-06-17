aed1d212cd365f1c3ab576b6a34532637bbabe4e
descallar24 <ralphronan5@gmail.com>
2022-06-17 21:25:13 +0800

    gateway

---
+++ app/Http/Controllers/User1Controller.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Response; // Response Components
+use App\Traits\ApiResponser; // <-- use to standardized 
+use Illuminate\Http\Request; // <-- handling http request 
+use App\Services\User1Service; // user1 Services
+Class User1Controller extends Controller {
+ // use to add your Traits ApiResponser
+ use ApiResponser;
+ /**
+ * The service to consume the User1 Microservice
+ * @var User1Service
+ */
+ public $user1Service;
+ /**
+ * Create a new controller instance
+ * @return void
+ */
+ public function __construct(User1Service 
+$user1Service){
+ $this->user1Service = $user1Service;
+ }
+ /**
+ * Return the list of users
+ * @return Illuminate\Http\Response
+ */
+ public function index()
+ {
+ // 
+ return $this->successResponse($this->user1Service->obtainUsers1()); 
+
+ }
+ public function add(Request $request ){
+    return $this->successResponse($this->user1Service->createUser1($request->all(), 
+
+   Response::HTTP_CREATED));
+    }
+    /**
+ * Obtains and show one user
+ * @return Illuminate\Http\Response
+ */
+ public function show($id)
+ {
+ return $this->successResponse($this->user1Service->obtainUser1($id));
+
+
+ }
+ public function update(Request $request,$id)
+ {
+ return $this->successResponse($this->user1Service->editUser1($request->all(), $id));
+
+
+ }
+ public function delete($id)
+ {
+ return $this->successResponse($this->user1Service->deleteUser1($id));
+
+ }
+}
